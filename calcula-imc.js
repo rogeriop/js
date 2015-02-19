b1c40f9ff347846ebdc52cbf420f914ffb4415cb
rogeriop <pinheiroxrogerio@gmail.com>
2015-02-19 14:36:55 -0300

    fim 04 Colecoes e loops

--- calcula-imc.js
+++ calcula-imc.js
@@ -1,11 +1,13 @@
-var peso = 75;
-var altura = 1.7;
-var paciente = "Lula Molusco"
-
+var paciente = {peso:75, altura:1.7, nome:"Lula Molusco"};
 var imc = 0;
 
-if (altura !== 0) {
-    imc = peso / (altura * altura);
+var peso = document.getElementById("peso-1").textContent;
+var altura = document.getElementById("altura-1").textContent;
+var nome = document.getElementById("nome-1").textContent;
+
+
+if (paciente.altura !== 0) {
+    imc = paciente.peso / (paciente.altura * paciente.altura);
 } else {
     console.log("altura tem que ser diferente de zero");
 }
