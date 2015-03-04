271e440d6ad712bd4684f0129454c047c0f19385
rogeriop <pinheiroxrogerio@gmail.com>
2015-03-04 08:30:49 -0300

    fim 05 Funcoes e os cidadaos de primeira classe

--- calcula-imc.js
+++ calcula-imc.js
@@ -1,26 +1,63 @@
+function montaPaciente (tr) {
 
-var trs = document.getElementsByClassName("paciente");
- 
-for(var posi = 0; posi <= trs.length - 1; posi++){
-     var nome = trs[posi].getElementsByClassName("info-nome")[0]; // pega tr do nome do paciente atual
-     var peso = trs[posi].getElementsByClassName("info-peso")[0]; // pega tr do peso do paciente atual
-     var altura = trs[posi].getElementsByClassName("info-altura")[0]; // pega tr do altura do paciente atual
-     var imc = trs[posi].getElementsByClassName("info-imc")[0]; // pega tr do imc do paciente atual
+     var nome = tr.getElementsByClassName("info-nome")[0]; // pega tr do nome do paciente atual
+     var peso = tr.getElementsByClassName("info-peso")[0]; // pega tr do peso do paciente atual
+     var altura = tr.getElementsByClassName("info-altura")[0]; // pega tr do altura do paciente atual
  
      var paciente = {
          nome : nome.textContent,
          peso : peso.textContent,
-         altura : altura.textContent
+         altura : altura.textContent,
+         pegaImc : function() {
+                        if(this.altura != 0){
+                             var imcDoPaciente = this.peso / (this.altura * this.altura);
+                             return imcDoPaciente;
+                         }else{
+                             console("altura tem que ser diferente de zero");
+                         }
+         
+                    }
+     
      }
- 
-     if(paciente.altura != 0){
-         var imcDoPaciente = paciente.peso / (paciente.altura * paciente.altura);
- 
-         imc.textContent = imcDoPaciente;
- 
-         console.log(imcDoPaciente); // 25
-     }else{
-         console("altura tem que ser diferente de zero");
-     }
- 
+    return paciente;
 }
+
+function percorreArray (array, comportamento) {
+
+    for(var posi = 0; posi <= trs.length - 1; posi++){
+    var trPaciente = trs[posi];
+    comportamento(trPaciente);
+    }
+}
+
+
+
+
+
+var trs = document.getElementsByClassName("paciente");
+ 
+percorreArray(trs, function(trPaciente){
+
+    var paciente = montaPaciente(trPaciente);
+    imcCalculado =  paciente.pegaImc();
+    var trImc = trPaciente.getElementsByClassName("info-imc")[0]; // pega tr do imc do paciente atual
+    trImc.textContent = imcCalculado;
+
+    console.log(imcCalculado); // 25
+    
+    
+});
+
+//for(var posi = 0; posi <= trs.length - 1; posi++){
+//
+//    var trPaciente = trs[posi];
+//    var trImc = trPaciente.getElementsByClassName("info-imc")[0]; // pega tr do imc do paciente atual
+//
+//    var paciente = montaPaciente(trPaciente);
+//    imcCalculado =  paciente.pegaImc();
+//    trImc.textContent = imcCalculado;
+//
+//    console.log(imcCalculado); // 25
+// 
+//}
+    
---
+++ percorre.js
@@ -0,0 +1,5 @@
+function percorreArray (array, comportamento) {
+    
+    
+    
+}
